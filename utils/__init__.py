8dfcd9022e2ad69682e3b6e8423d9c9bde312efd
ZhanYang-nwpu <1242387556@qq.com>
2024-03-13 10:54:27 +0800

    utils

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+# ------------------------------------------------------------------------
+# Mono3DVG (https://github.com/ZhanYang-nwpu/Mono3DVG)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# ------------------------------------------------------------------------
+
